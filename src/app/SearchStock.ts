2b0181f11d99bd5b13d332e06f42ebb92c57d8d4
SaiKumar <saikumar.maroj.999@gmail.com>
2019-08-03 19:08:28 +0530

    Stock App

---
+++ src/app/SearchStock.ts
@@ -0,0 +1,7 @@
+import { Search } from './search';
+export const SEARCH : Search[] = [
+	{SearchElement:'INFY', val:[10,50.30,90,100],SearchTime:''},
+	{SearchElement:'TCS',val:[161.5,200.70,1000,900],SearchTime:''},
+	{SearchElement:'CTS',val:[123,32,124.65,231.70],SearchTime:''},
+	{SearchElement:'IBM',val:[100,200,300,400],SearchTime:''}
+];
